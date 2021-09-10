67be50f11be4cafb0576f4316166b649f2f48f61
ndz <romanyuzhanin84@gmail.com>
2021-09-10 10:54:43 +0300

    tests

---
+++ api/tests/Functional/HomeTest.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace Test\Functional;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Slim\App;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Psr7\Factory\ServerRequestFactory;
+use Slim\Factory\AppFactory;
+
+class HomeTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $response = $this->app()->handle(self::json('GET', '/'));
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+        self::assertEquals('{}', (string)$response->getBody());
+    }
+
+    protected function app(): App
+    {
+        $container = require __DIR__.'/../../config/container.php';
+
+        $app = AppFactory::createFromContainer($container);
+
+        (require __DIR__ . '/../../config/middleware.php')($app, $container);
+        (require __DIR__ . '/../../config/routes.php')($app);
+
+        return $app;
+    }
+
+    protected static function request(string $method, string $path): ServerRequestInterface
+    {
+        return (new ServerRequestFactory())->createServerRequest($method, $path);
+    }
+
+    protected static function json(string $method, string $path): ServerRequestInterface
+    {
+        return self::request($method, $path)
+            ->withHeader('Accept', 'application/json')
+            ->withHeader('Content-Type', 'application/json');
+    }
+}
--- api/tests/Unit/Http/JsonResponseTest.php
+++ api/tests/Unit/Http/JsonResponseTest.php
@@ -1,6 +1,65 @@
 <?php
+declare(strict_types=1);
 
-class JsonResponseTest
+namespace Test\Unit\Http;
+
+use App\Http\JsonResponse;
+use PHPUnit\Framework\TestCase;
+
+class JsonResponseTest extends TestCase
 {
+    public function testInt(): void
+    {
+        $response = new JsonResponse(12);
 
+        self::assertEquals('12', $response->getBody()->getContents());
+        self::assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testString(): void
+    {
+        $response = new JsonResponse('12');
+
+        self::assertEquals('"12"', $response->getBody()->getContents());
+        self::assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testIntWithCode(): void
+    {
+        $response = new JsonResponse(12, 201);
+
+        self::assertEquals('12', $response->getBody()->getContents());
+        self::assertEquals(201, $response->getStatusCode());
+    }
+
+    public function testNull(): void
+    {
+        $response = new JsonResponse(null);
+
+        self::assertEquals('null', $response->getBody()->getContents());
+        self::assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testObject(): void
+    {
+        $object = new \StdClass();
+        $object->str = 'value';
+        $object->int = 1;
+        $object->none = null;
+
+        $response = new JsonResponse($object, 200);
+
+        self::assertEquals('{"str":"value","int":1,"none":null}', $response->getBody()->getContents());
+        self::assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testArray(): void
+    {
+        $array = ['str'=>'value', 'int'=>1, 'none'=>null];
+
+        $response = new JsonResponse($array, 200);
+
+        self::assertEquals('{"str":"value","int":1,"none":null}', $response->getBody()->getContents());
+        self::assertEquals(200, $response->getStatusCode());
+    }
 }
