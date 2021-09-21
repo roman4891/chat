afbb38f3029312742293926900e77511119a12d9
ndz <romanyuzhanin84@gmail.com>
2021-09-21 18:40:18 +0300

    updated index.php

--- api/public/index.php
+++ api/public/index.php
@@ -2,19 +2,16 @@
 
 declare(strict_types=1);
 
-use Slim\Factory\AppFactory;
-use App\Http;
+use Psr\Container\ContainerInterface;
+use Slim\App;
 
 http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+/** @var ContainerInterface $container */
 $container = require __DIR__ . '/../config/container.php';
 
-$app = AppFactory::createFromContainer($container);
-
-(require __DIR__ . '/../config/middleware.php')($app, $container);
-(require __DIR__ . '/../config/routes.php')($app);
-
-
+/** @var App $app */
+$app = (require __DIR__ . '/../config/app.php')($container);
 $app->run();
